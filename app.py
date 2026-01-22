3514218b55c288f193bcc7a7116af09c593c6f52
mdsaibhossain <saibhossain5@gmail.com>
2026-01-23 03:08:11 +0600

    main

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from models.transition_model import TransitionModel
+from embeddings.vector_store import VectorStore
+from agent.rag_agent import CustomerRAGAgent
+
+df = pd.read_csv("data/transactions.csv")
+#print(df)
+df = df.sort_values(["customer_id","timestamp"])
+print(df)
+
+sequences = (
+    df.groupby("customer_id")["item"].apply(list).tolist()
+)
+
+tm = TransitionModel()
+tm.train(sequences)
+
+vs = VectorStore()
+vs.add(["-> ".join(seq) for seq in sequences])
+
+agent = CustomerRAGAgent(vs,tm)
+
+history = ["Bread","Milk"]
+result = agent.run(history)
+print(result)
