d61057db64ee3741561e5a7244ae5638a1e20087
mdsaibhossain <saibhossain5@gmail.com>
2026-01-23 13:09:15 +0600

    12 fetures datset

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from models.transition_model import TransitionModel
 from embeddings.vector_store import VectorStore
 from agent.rag_agent import CustomerRAGAgent
 
-df = pd.read_csv("data/transactions.csv")
+df = pd.read_csv("data/update_dataset11.csv")
 #print(df)
 df = df.sort_values(["customer_id","timestamp"])
-print(df)
+print(df[["customer_id","item_sequence","item","category","price"]])
 
 sequences = (
     df.groupby("customer_id")["item"].apply(list).tolist()
